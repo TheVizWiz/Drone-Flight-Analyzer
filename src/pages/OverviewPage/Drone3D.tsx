5398c61fd239ef4160a9029eca43321b8a654d83
TheVizWiz <vibhusiyer@gmail.com>
2022-05-02 03:24:35 -0400

    changed to electron

---
+++ src/pages/OverviewPage/Drone3D.tsx
@@ -0,0 +1,55 @@
+import React, {useContext, useRef, useState} from "react";
+import {Canvas, useFrame} from "@react-three/fiber";
+import "scss/Drone3D.scss"
+import {DroneDataContext} from "../../data/DroneDataContext";
+
+
+export default function Drone3D (props: any) {
+    let canvas = (
+        <Canvas>
+            <ambientLight intensity={0.1}/>
+            <directionalLight color="red" position={[0, 0, 5]}/>
+            <mesh>
+                <boxGeometry/>
+                <meshStandardMaterial/>
+            </mesh>
+        </Canvas>);
+
+
+
+
+    return (
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    );
+}
+
+function Box(props: JSX.IntrinsicElements['mesh']) {
+    const ref = useRef<THREE.Mesh>(null!)
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+
+
+
+    useFrame((state, delta) => {
+        (ref.current.rotation.x += 0.01)
+    })
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => {
+                click(!clicked)
+            }}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
--- src/pages/OverviewPage/OverviewPage.tsx
+++ src/pages/OverviewPage/OverviewPage.tsx
@@ -1,9 +1,49 @@
-import React, {Component} from "react";
+import React, {useContext} from "react";
+import Panel from "../components/Panel";
+import Drone3D from "./Drone3D";
+import "scss/OverviewPage.scss"
+import ChartPanel from "../components/ChartPanel";
+import {DroneData, DroneDataContext} from "../../data/DroneDataContext";
 
 
-export default class OverView extends Component<any, any> {
-    render () {
-        return <div>Overview</div>;
-    }
+interface OverviewProps {
+
+}
+
+
+export default function OverviewPage (props: OverviewProps) {
+
+    const data = useContext(DroneDataContext);
+
+    let panels = [
+        <Panel title={"Current Direction"} content={<Drone3D/>} dim={[1, 7, 4, 10]}/>,
+        //left panel
+        <Panel title={"Roll"} dim={[1, 5, 1, 4]}
+               content={<ChartPanel xData={DroneData.time} yData={DroneData.roll}/>}/>,
+        <Panel title={"Pitch"} dim={[5, 9, 1, 4]}
+               content={<ChartPanel xData={DroneData.time} yData={DroneData.pitch}/>}/>,
+        <Panel title={"Yaw"} dim={[9, 13, 1, 4]} content={<ChartPanel xData={DroneData.time} yData={DroneData.yaw}/>}/>,
+        // right panel
+        <Panel title={"Network"} dim={[1, 5, 10, 13]}
+               content={<ChartPanel xData={DroneData.time} yData={DroneData.network}/>}/>,
+        <Panel title={"Radio"} dim={[5, 9, 10, 13]}
+               content={<ChartPanel xData={DroneData.time} yData={DroneData.radio}/>}/>,
+        <Panel title={"Battery"} dim={[9, 13, 10, 13]}
+               content={<ChartPanel xData={DroneData.time} yData={DroneData.battery}/>}/>,
+        // bottom middle
+        <Panel title={"State"} dim={[7, 10, 4, 7]}/>,
+        <Panel title={"Velocity"} dim={[7, 10, 7, 10]}/>,
+        <Panel title={"Payload"} dim={[10, 13, 4, 7]}/>,
+        <Panel title={"Extras"} dim={[10, 13, 7, 10]}/>,
+    ];
+
+    return (
+
+        <div className={"page"}>
+            <div className={"overviewGrid"}>
+                {panels}
+            </div>
+        </div>
+    )
 
 }
